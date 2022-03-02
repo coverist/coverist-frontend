7e26a0014b3fd0fc2f117ad89d1c943fea694ee6
donguk <dragon071@naver.com>
2022-03-02 23:43:07 +0900

    220302 JY's code merge

--- lib/models/provider.dart
+++ lib/models/provider.dart
@@ -6,26 +6,40 @@ import 'package:coverist/widgets/book_info/genre_element/genre_list.dart';
 class bookInfo with ChangeNotifier{
   String _title = 'nan';
   String _author = 'nan';
-  List<String> _gerne = [];
+  List<String> _genre = [];  //장르만, 전해주면 되니 string
   List<String> _tag = [];
 
   String get title => _title; //외부에서 접근이 가능하도록
   String get author => _author;
-  List<String> get genre => _gerne;
+  List<String> get genre => _genre;
   List<String> get tag => _tag;
 
-  void setTitle(title){
+  void setTitle(String title){
     _title = title;
     notifyListeners(); //위젯을 변화를 통보한다
   }
-  void setAuthor(author){
+  void setAuthor(String author){
     _author = author;
     notifyListeners(); 
   }
-  void setGenre(){
+  void setGenre(String addgenre){
+    _genre.add(addgenre); 
+    notifyListeners(); 
     print('wait');
   }
-  void setTag(){
+  void setTag(String addTag){
+    _genre.add(addTag);
+    notifyListeners(); 
     print('wait');    
   }  
+  void delGenre(String delgenre){
+    _genre.remove(delgenre);    
+    notifyListeners();  
+    print('wait');
+  }
+  void delTag(String delTag){
+    _genre.remove(delTag);
+    notifyListeners();     
+    print('wait');    
+  } 
 }
--- lib/screens/book_info/components/info_tag.dart
+++ lib/screens/book_info/components/info_tag.dart
@@ -1,24 +1,9 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MyApp()); //반드시 widget을 argument로 가져야 함
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: InfoTag(),
-      ),
-      theme: ThemeData(primarySwatch: Colors.blueGrey),
-    );
-  }
-}
+import '/widgets/book_info/input_text_widget.dart';
 
 class InfoTag extends StatefulWidget {
-  InfoTag({Key? key}) : super(key: key);
+  final event;
+  const InfoTag({Key? key, required this.event}) : super(key: key);
 
   @override
   _InfoTagState createState() => _InfoTagState();
@@ -42,10 +27,6 @@ class _InfoTagState extends State<InfoTag> {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        // Future.delayed(Duration(seconds: 3), () {
-        //   Navigator.pop(context);
-        // });
-        // return object of type Dialog
         return AlertDialog(
           content: new Text(text),
           actions: <Widget>[
@@ -61,34 +42,19 @@ class _InfoTagState extends State<InfoTag> {
     );
   }
 
-  Widget buildChips() {
+  List<Widget> buildChips() {
     List<Widget> chips = [];
 
     for (int i = 0; i < _values.length; i++) {
-      if (i == 5) {
-        _showDialog("최대 5개까지 입력할 수 있습니다."); //입력 개수 제한
-        break;
-      }
-
       InputChip actionChip = InputChip(
-        // selected: _selected[i],
         label: Text(_values[i]),
-        // avatar: FlutterLogo(),
-        // elevation: 10,
-        // pressElevation: 5,
+        elevation: 5,
         shadowColor: Colors.blueGrey,
-        // onPressed: () {
-        //   setState(() {
-        //     _selected[i] = !_selected[i];
-        //   });
-        // },
         onDeleted: () {
           _values.removeAt(i);
-          // _selected.removeAt(i);
 
           setState(() {
             _values = _values;
-            // _selected = _selected;
           });
         },
       );
@@ -96,91 +62,63 @@ class _InfoTagState extends State<InfoTag> {
       chips.add(actionChip);
     }
 
-    return ListView(scrollDirection: Axis.horizontal, children: chips);
+    return chips;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('INFO_TAG'),
-          centerTitle: true,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        SizedBox(height: 10),
+        InputText(
+          string: "태그",
         ),
-        backgroundColor: Colors.blueGrey[200],
-        body: Center(
-          child: Container(
-              height: (MediaQuery.of(context).size.height) * 0.7,
-              width: (MediaQuery.of(context).size.width) * 0.8,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Column(
-                    children: [
-                      Row(
-                        children: <Widget>[
-                          Container(
-                            child: Text('태그 입력',
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                    letterSpacing: 1.5)),
-                          ),
-                          Container(
-                            child: Text('(필수)',
-                                style: TextStyle(
-                                    fontSize: 14, letterSpacing: 1.5)),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 10),
-                      TextField(
-                          textInputAction: TextInputAction.go,
-                          controller: _infoTextEditController,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: '입력 예) #창업',
-                          ),
-                          onSubmitted: (value) {
-                            if ('${_infoTextEditController.text}'
-                                .startsWith("#")) {
-                              _values.add(_infoTextEditController.text);
-                              // _selected.add(true);
-                              _infoTextEditController.clear();
-                              print('values : $_values');
-                              setState(() {
-                                // _infoTextEditController.text = value;
-                                // inputTagList.add(value);
-                                _values = _values;
-                                // _selected = _selected;
+        // TextField(
+        //     textInputAction: TextInputAction.go,
+        //     controller: _infoTextEditController,
+        //     decoration: InputDecoration(
+        //       border: OutlineInputBorder(),
+        //       labelText: '입력 예) #창업',
+        //     ),
+        //     onSubmitted: (value) {
+        //       if ('${_infoTextEditController.text}'.startsWith("#")) {
+        //         _values.add(_infoTextEditController.text);
+        //         // _selected.add(true);
+        //         _infoTextEditController.clear();
+        //         print('values : $_values');
+        //         setState(() {
+        //           // _infoTextEditController.text = value;
+        //           // inputTagList.add(value);
+        //           _values = _values;
+        //           // _selected = _selected;
 
-                                print('value : $value');
-                              });
-                            } else {
-                              _showDialog("입력 형식이 올바르지 않습니다.");
-                              // print('#태그 형식이 아님');
-                              //alertDialog 띄워야 함
-                            }
-                          }),
+        //           print('value : $value');
+        //         });
+        //       } else {
+        //         _showDialog("입력 형식이 올바르지 않습니다.");
+        //         // print('#태그 형식이 아님');
+        //         //alertDialog 띄워야 함
+        //       }
+        //     }),
+        SizedBox(height: 10),
 
-                      // buildChips(),
+        // buildChips(),
 
-                      // Row(children: <Widget>[
-                      //   new TagWidget(
-                      //       string: '${_infoTextEditController.text}'),
-                      // ]),
-                      Column(children: <Widget>[
-                        Container(
-                          height: 200,
-                          margin:
-                              const EdgeInsets.only(left: 20.0, right: 20.0),
-                          child: buildChips(),
-                        )
-                      ]),
-                    ],
-                  ),
-                ],
-              )),
-        ));
+        // Row(children: <Widget>[
+        //   new TagWidget(
+        //       string: '${_infoTextEditController.text}'),
+        // ]),
+        Wrap(
+            direction: Axis.horizontal, // 정렬 방향
+            alignment: WrapAlignment.center, // 정렬 방식
+            spacing: 10, // 상하(좌우) 공간
+            runSpacing: 10, // 좌우(상하) 공간
+            children: buildChips()),
+      ],
+      // ),
+      // ],
+    );
   }
 }
