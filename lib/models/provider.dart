aa98052984ee11cb050daf513fe069f7e21863a5
yeonnn03 <yeoni0324@nate.com>
2022-06-16 19:10:04 +0900

    Modify bug(재생성 시 입력값 초기화)

--- lib/models/provider.dart
+++ lib/models/provider.dart
@@ -10,23 +10,25 @@ class BookInfo with ChangeNotifier {
   List<List<Coverinfo>> allCoverInfo = [];
   List<String> allItemList = [];
 
-  // String _title = 'nan';
-  // String _author = 'nan';
+  List<Coverinfo> coverinfoList = [];
 
-  // String _genre = 'nan';
-  // String _subgenre = 'nan';
+  String _title = 'nan';
+  String _author = 'nan';
 
-  // String _publisher = ' ';
+  String _genre = 'nan';
+  String _subgenre = 'nan';
 
-  // List<String> _tag = [];
+  String _publisher = ' ';
+
+  List<String> _tag = [];
 
 // default 고정
-  String _title = '마지막 벚꽃이 질 때';
-  String _author = '김수민';
-  String _genre = '시/에세이';
-  String _subgenre = '나라별 에세이';
-  String _publisher = ' ';
-  List<String> _tag = ['벚꽃', '이야기'];
+  // String _title = '마지막 벚꽃이 질 때';
+  // String _author = '김수민';
+  // String _genre = '시/에세이';
+  // String _subgenre = '나라별 에세이';
+  // String _publisher = ' ';
+  // List<String> _tag = ['벚꽃', '이야기'];
 
   String get title => _title; //외부에서 접근이 가능하도록
   String get author => _author;
@@ -41,70 +43,57 @@ class BookInfo with ChangeNotifier {
   void setTitle(String title) {
     _title = title;
     notifyListeners();
-    // print("provider title : " + title);
   }
 
   void setAuthor(String author) {
     _author = author;
     notifyListeners();
-    // print("provider author : " + author);
   }
 
   void setGenre(String addgenre) {
     _genre = addgenre;
     notifyListeners();
-    // print(_genre);
   }
 
   void setSubGenre(String addgenre) {
     _subgenre = addgenre;
     notifyListeners();
-    // print(_subgenre);
   }
 
   void setTag(List<String> addTag) {
     _tag = addTag;
     notifyListeners();
-    // for (int i = 0; i < _tag.length; i++)
-    // print("provider tag : " + addTag.toString());
   }
 
   void delTag(String delTag) {
     _tag.remove(delTag);
     notifyListeners();
-    // print('wait');
   }
 
   void setPublisher(String data) {
     _publisher = data;
     notifyListeners();
-    // print('publisher : complete');
   }
 
   void setAllList(List<Coverinfo> ci, int num) {
     print("setalllist num : $num");
 
-    // if (seturl.compareTo('') == 0)
+    //해당 입력 정보로 처음 생성 시
     if (num == -1) {
-      print("allcoverinfo add전");
-
+      print("-1 allcoverinfo add전 : ${allCoverInfo.toString()}");
       allCoverInfo.add(ci);
-      print("allcoverinfo add됨");
+      print("-1 allcoverinfo add됨 : ${allCoverInfo.toString()}");
+
+      //해당 입력 정보로 생성된 표지가 있을 때 bookid 비교해서 추가
     } else if (allCoverInfo[num][0].bookid == ci[num].bookid) {
-      print("setalllist num : $num");
       print(
           "allCoverInfo[0][0].bookid : ${allCoverInfo[0][0].bookid.toString()}");
       print(
           "allCoverInfo[num][0].bookid : ${allCoverInfo[num][0].bookid.toString()}");
-
-      print(ci[num].url);
-      allCoverInfo[num][0].url = ci[num].url;
-    } else {
-      print("ci[num].bookid : ${ci[num].bookid}");
+      allCoverInfo[num].addAll(ci);
     }
 
-    // print('allCoverInfo : $allCoverInfo[0][0].url.toString()');
-    print('\nlen(allCoverInfo) : ${allCoverInfo.length}');
+    print('\n allCoverInfo.length : ${allCoverInfo.length}');
   }
 
   List<String> getAllItem() {
@@ -117,7 +106,16 @@ class BookInfo with ChangeNotifier {
     return allItemList;
   }
 
+  List<Coverinfo> setAllInfo() {
+    return coverinfoList;
+  }
+
   List<List<Coverinfo>> getAllInfo() {
+    print("getAllInfo의 allCoverInfo.toString() : ${allCoverInfo.toString()}");
+    return allCoverInfo;
+  }
+
+  Future<List<List<Coverinfo>>> getFutureInfo() async {
     return allCoverInfo;
   }
 
@@ -142,41 +140,39 @@ class BookInfo with ChangeNotifier {
       'tags': formdataTag,
       'publisher': _publisher
     });
-    String api_path = "http://3.37.43.37:8080/api/v1/book" + seturl;
+    // String api_path = "http://3.37.43.37:8080/api/v1/book" + seturl;
+    String api_path = "http://43.200.53.170:8080/api/v1/book" + seturl;
     var response = await dio.post(api_path, data: formData);
-
-    // print('response.toString() : $response.toString()');
-    // print('\nresponse.data[1]) : $response.data[1]');
-//    //{cover_id: 0, book_id: 0, title: a, author: b, genre: 컴퓨터/IT, sub_genre: 모바일프로그래밍, tags: [dd], publisher: Coverist, url: https://coverist.s3.ap-northeast-2.amazonaws.com/XL.jpg, created_date: 2022-01-01T00:00:00}
-    // print('\nresponse.data.length : $response.data.length');
 
     var coverData = List<Coverinfo>.generate(response.data.length,
         (index) => Coverinfo.fromJson(response.data[index]));
 
-    print("coverData : $coverData");
-    // print('\ncoverData[0] : ');
-    // print(coverData[0]); //Instance of 'Coverinfo'
-    // print('\ncoverData[0].url.toString() : ');
-    // print(coverData[0].url.toString()); //https://coverist.s3.ap-northeast-2.amazonaws.com/XL.jpg
-
     setAllList(coverData, num);
+
+    // 입력값 초기화
+    _title = 'nan';
+    _author = 'nan';
+    _genre = 'nan';
+    _subgenre = 'nan';
+    _publisher = ' ';
+    _tag = [];
 
     return coverData;
   }
 
   String nullcheck() {
     if (_title == 'nan') {
-      return "title";
+      return "제목을 입력";
     } else if (_author == 'nan') {
-      return "author";
+      return "저자명을 입력";
     } else if (_tag == []) {
-      return "tag";
+      return "태그를 입력";
     } else if (_genre == 'nan') {
-      return "genre";
+      return "대분류를 선택";
     } else if (_subgenre == 'nan') {
-      return "subgenre";
+      return "중분류를 선택";
     } else if (_tag.isEmpty) {
-      return "tag";
+      return "태그를 입력";
     } else {
       return "allpass";
     }
